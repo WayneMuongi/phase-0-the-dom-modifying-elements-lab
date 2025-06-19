1b386e775decf765546603b55becdd9f18c5c7e4
WayneMuongi <waynemuongi77@gmail.com>
2025-06-19 17:52:26 +0300

    modifying the dom

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const main= document.getElementById("main");
+main.remove();
+
+let newHeader=document.createElement("h1")
+newHeader.id="victory";
+newHeader.innerHTML="Wayne is the champion";
